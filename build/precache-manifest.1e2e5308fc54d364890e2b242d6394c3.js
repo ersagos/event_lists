b5377d413fc61f4e5fa7705944b661fe47292381
ersagos <j.grall3@gmail.com>
2020-08-23 19:17:31 +0200

    Add docker image

---
+++ build/precache-manifest.1e2e5308fc54d364890e2b242d6394c3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0db125ab7696092e87501824b5ca2a26",
+    "url": "/index.html"
+  },
+  {
+    "revision": "31bd6f5458248b4c0a08",
+    "url": "/static/css/main.f2060688.chunk.css"
+  },
+  {
+    "revision": "2aa9d15e90cee114b58f",
+    "url": "/static/js/2.7fbdfd5c.chunk.js"
+  },
+  {
+    "revision": "c87550333da8df8ab4c80bd1d6e9defb",
+    "url": "/static/js/2.7fbdfd5c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "31bd6f5458248b4c0a08",
+    "url": "/static/js/main.640aa2f0.chunk.js"
+  },
+  {
+    "revision": "73635f645c271e35ff15",
+    "url": "/static/js/runtime-main.83c3e0c4.js"
+  }
+]);
